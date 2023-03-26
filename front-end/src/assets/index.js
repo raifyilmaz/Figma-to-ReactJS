4105423a2b7a80dec49fc7a40d47801f2e51b728
raifyilmaz <21raif14@gmail.com>
2023-03-26 01:12:09 -0400

    build-up

---
+++ front-end/src/assets/index.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg'
+import headerBg from 'headerBg.png'
+import topics1 from './topics1.png'
+import topics2 from './topics2.png'
+import topics3 from './topics3.png'
+import topics4 from './topics4.png'
+import topics5 from './topics5.png'
+import topics6 from './topics6.png'
+import student from './student.svg'
+import file from './file.svg'
+import blog1 from './blog1.png'
+import blog2 from './blog2.png'
+import blog3 from './blog3.png'
+import peterAdams from './peterAdams.png'
+import emilySmith from './emilySmith.png'
+import robertFox from './robertFox.png'
+import facebookLogo from './facebookLogo.svg'
+import instagramLogo  from './instagramLogo.svg'
+import twitterLogo from './twitterLogo.svg'
+
+export {
+    logo, headerBg, /*Header*/
+    topics1, topics2, topics3, topics4, topics5, topics6, /*Topics*/
+    student, file, /*Info*/
+    blog1, blog2, blog3, /*Blog*/ 
+    peterAdams, emilySmith, robertFox, /*Testimonials*/
+    facebookLogo, twitterLogo, instagramLogo /*Footer*/
+}
---
+++ front-end/src/components/index.js
@@ -0,0 +1,7 @@
+import Blog from './Blog/Blog'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Info from './Info/Info'
+import Testimonials from './Testimonials/Testimonials'
+import Topics from './Topics/Topics'
+export {Blog, Footer, Header, Info, Testimonials, Topics}
