66609a347635bf5d2c8be3da0d65833cd4391ed0
raifyilmaz <21raif14@gmail.com>
2023-03-27 14:44:37 -0400

    Testimonials

--- front-end/src/components/Testimonials/Testimonials.jsx
+++ front-end/src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './Testimonials.css'
+import {peterAdams, emilySmith, robertFox} from '../../assets'
 
 const Testimonials = () => {
     return(
-        <div>Testimonials</div>
+        <section id="testimonials" className="dark-gray">
+            <div className="wrapper">
+                <h2>What our students say?</h2>
+                <div className="content-container">
+                    <div className="testimonial">
+                        <img src={peterAdams} />
+                        <div className="reviewer-details">
+                            <div className="name">Peter Adams</div>
+                            <div className="company-name">Google</div>
+                            <div className="review">This is a great course. I got to learn a lot.</div>
+                        </div>
+                    </div>
+
+                    <div className="testimonial">
+                        <img src={robertFox} />
+                        <div className="reviewer-details">
+                            <div className="name">Robert Fox</div>
+                            <div className="company-name">Instagram</div>
+                            <div className="review">I got to learn a lot about Music Production with this course. Thanks :)</div>
+                        </div>
+                    </div>
+
+                    <div className="testimonial">
+                        <img src={emilySmith} />
+                        <div className="reviewer-details">
+                            <div className="name">Emily Smith</div>
+                            <div className="company-name">Apple</div>
+                            <div className="review">Awesome! Great job!!</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
